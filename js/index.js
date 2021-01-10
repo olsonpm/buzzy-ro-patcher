c47bbf2a0f278b431ddbf57bb819a0e0cc2ca7f1
Phil Olson <philip.olson@pm.me>
2021-01-10 09:33:42 -0600

    misc updates
    - fix menu buttons to take up full width instead of li
    - add error alert
    - default play button to 'cancel'
    - add auto formatting and eslint
    - fix menu to disappear when user clicks outside of the menu

--- js/index.js
+++ js/index.js
@@ -1,60 +1,86 @@
-'use strict';
+'use strict'
 
-const bar1 = new ldBar("#loading-bar")
-bar1.set(100)
-
-const hamburger = document.getElementById('hamburger'),
+const btn = getButtons(),
+  errorAlert = document.getElementById('error-alert'),
+  errorMsg = document.getElementById('error-msg'),
+  loadingBar = new ldBar('#loading-bar'),
+  hamburger = document.getElementById('hamburger'),
   menu = document.getElementById('menu')
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active')
   menu.classList.toggle('hidden')
-});
+})
+
+// external.invoke('start_update')
+
+document.addEventListener('click', evt => {
+  if (
+    !menu.classList.contains('hidden') &&
+    !menu.contains(evt.target) &&
+    !hamburger.contains(evt.target)
+  ) {
+    menu.classList.add('hidden')
+    hamburger.classList.remove('active')
+  }
+})
+
+function getButtons() {
+  return {
+    main: document.getElementById('main-btn'),
+    resetCache: document.getElementById('reset-cache'),
+    setup: document.getElementById('setup'),
+  }
+}
+
+/* eslint-disable no-unused-vars */
+
+function patchingStatusReady() {
+  loadingBar.set(100)
+  btn.main.textContent = 'Play'
+}
+
+function patchingStatusError(msg) {
+  errorMsg.textContent = msg
+  errorAlert.classList.remove('removed')
+  errorAlert.classList.remove('hidden')
+}
 
 /*
 
-external.invoke('start_update');
-
-function patchingStatusReady() {
-  $("#download-progress-bar")
-      .css("width", "100%")
-      .attr("aria-valuenow", "100")
-      .removeClass("bg-warning")
-      .addClass("bg-success");
-  $("#download-progress-text").text("Ready");
-  $("#button-play").prop('disabled', false);
-}
-
-function patchingStatusError(errorMsg) {
-  $("#download-progress-bar")
-      .css("width", "100%")
-      .attr("aria-valuenow", "100")
-      .removeClass("bg-warning")
-      .addClass("bg-danger");
-  $("#download-progress-text").text("Failure: " + errorMsg);
-}
-
 function patchingStatusDownloading(nbDownloaded, nbTotal, bytesPerSec) {
-  var percentage = (100 * nbDownloaded) / nbTotal;
+  var percentage = (100 * nbDownloaded) / nbTotal
   if (bytesPerSec > 0) {
-      var downloadSpeed = " - " + humanFileSize(bytesPerSec) + "/s";
+    var downloadSpeed = ' - ' + humanFileSize(bytesPerSec) + '/s'
   } else {
-      var downloadSpeed = "";
+    var downloadSpeed = ''
   }
-  $("#download-progress-bar").css("width", percentage + "%").attr("aria-valuenow", percentage);
-  $("#download-progress-text").text("Downloading: " + nbDownloaded + "/" + nbTotal + downloadSpeed);
+  $('#download-progress-bar')
+    .css('width', percentage + '%')
+    .attr('aria-valuenow', percentage)
+  $('#download-progress-text').text(
+    'Downloading: ' + nbDownloaded + '/' + nbTotal + downloadSpeed
+  )
 }
 
 function patchingStatusInstalling(nbInstalled, nbTotal) {
-  var percentage = (100 * nbInstalled) / nbTotal;
-  $("#download-progress-bar").css("width", percentage + "%").attr("aria-valuenow", percentage);
-  $("#download-progress-text").text("Installing: " + nbInstalled + "/" + nbTotal);
+  var percentage = (100 * nbInstalled) / nbTotal
+  $('#download-progress-bar')
+    .css('width', percentage + '%')
+    .attr('aria-valuenow', percentage)
+  $('#download-progress-text').text(
+    'Installing: ' + nbInstalled + '/' + nbTotal
+  )
 }
 
 // Note: Function taken from https://stackoverflow.com/a/20732091
 function humanFileSize(size) {
-  var i = size == 0 ? 0 : Math.floor(Math.log(size) / Math.log(1024));
-  return (size / Math.pow(1024, i)).toFixed(2) * 1 + ' ' + ['B', 'kiB', 'MiB', 'GiB', 'TiB'][i];
+  var i = size == 0 ? 0 : Math.floor(Math.log(size) / Math.log(1024))
+  return (
+    (size / Math.pow(1024, i)).toFixed(2) * 1 +
+    ' ' +
+    ['B', 'kiB', 'MiB', 'GiB', 'TiB'][i]
+  )
 }
 
 */
